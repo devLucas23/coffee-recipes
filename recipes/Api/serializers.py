d5abbed273fee63144f1189477c49807f3cdbd4f
EmsonFranca <esf12364@gmail.com>
2024-03-19 17:22:30 -0300

    update: Model end Serializers the recipes

--- recipes/Api/serializers.py
+++ recipes/Api/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
-from recipes.models import Recipes
+from recipes.models import Recipes, Ingredient
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ['name', 'quantify']
 
 class RecipeSerializer(serializers.ModelSerializer):
+    ingredients = IngredientSerializer(many=True)
+
     class Meta:
         model = Recipes
-        fields  = ['id','title','description' 'preparation','intensidade']
+        fields = ['id', 'title', 'intensity', 'description', 'preparation', 'ingredients']
+    
+    def create(self, validated_data):
+        ingredients_data = validated_data.pop('ingredients')
+        recipe = Recipes.objects.create(**validated_data)
+        for ingredient_data in ingredients_data:  # Corrigir o nome da variável
+            Ingredient.objects.create(recipe=recipe, **ingredient_data)
+        return recipe
--- recipes/models.py
+++ recipes/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class Recipes(models.Model):
+class Recipes (models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=100)
-    preparation = models.CharField(max_length=100)
-    intensidade = models.IntegerField()
+    intensity = models.IntegerField(default=1)
+    description = models.TextField()
+    preparation = models.TextField()
 
-    def __str__(self):
+    def _str_(self):
         return self.title
+class Ingredient(models.Model):
+    recipe = models.ForeignKey(Recipes, related_name='ingredients', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    quantify = models.IntegerField(default=1)
+
+    def _str_(self):
+        return self.name    
