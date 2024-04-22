86ac2011d073f4e9addec3b1f29b83489e1cf214
Lucas Anderson <lucas.ferreira24241@alunos.ufersa.edu.br>
2024-04-22 17:45:27 -0300

    Atualização do database

--- coffee/settings.py
+++ coffee/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
-import boto3
-from storages.backends.s3boto3 import S3Boto3Storage
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,13 +80,28 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'coffee.wsgi.application'
 
-
+'''
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'coffee',
+        'USER': 'grupo',
+        'PASSWORD': 'coffee123123@',
+        'HOST': 'localhost',  # ou o endereço do seu servidor MySQL
+        'PORT': '3306',  # porta padrão do MySQL
+        'OPTIONS' : {
+        'charset': 'utf8mb4',
+        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
